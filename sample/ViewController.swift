5b092601bd6100b1ec57e1339abf7f31b8b35034
Apple Macbook <applemacbook@mahesh.local>
2017-03-18 15:59:49 +0530

    changed strings

--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var str:String = ""
+        var str1:String = ""
+
         print("str")
         // Do any additional setup after loading the view, typically from a nib.
     }
